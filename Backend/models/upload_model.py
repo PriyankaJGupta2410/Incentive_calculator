20a698b568a73a94cf1cb568b07aaf0e1aaf800d
PriyankaGupta2410 <69896229+PriyankaJGupta2410@users.noreply.github.com>
2026-03-17 23:41:30 +0530

    commit

--- Backend/models/upload_model.py
+++ Backend/models/upload_model.py
@@ -1,7 +1,8 @@
 class upload:
-    def __init__(self,file_name,file_path,total_records,invalid_rows_count,invalid_rows):
+    def __init__(self,file_name,file_path,total_records,invalid_rows_count,invalid_rows,file_type):
         self.file_name = file_name
         self.file_path = file_path
         self.total_records = total_records
         self.invalid_rows_count = invalid_rows_count
         self.invalid_rows = invalid_rows
+        self.file_type = file_type
